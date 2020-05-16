facf0beda973d2414440d8ad456696af1f84b169
Vitaly <kaliummati@gmail.com>
2020-05-16 23:42:33 +0900

    изменение настроек для sidekiq: подключение к redis в docker контейнере

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:8081/rails-openssl-monitor' }
+  config.redis = { url: 'redis://redis/rails-openssl-monitor' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:8081/rails-openssl-monitor' }
+  config.redis = { url: 'redis://redis/rails-openssl-monitor' }
 end
